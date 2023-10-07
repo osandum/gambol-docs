bfb555a7ed0adaa67a8d736ed945c8c78e478363
Ole Sandum <ole@sandum.net>
2023-10-07 16:22:28 +0200

    (small rename)

--- src/main/java/net/sandum/xml/XmlDateTimeAdapter.java
+++ src/main/java/net/sandum/xml/XmlDateTimeAdapter.java
@@ -1,7 +1,5 @@
 package net.sandum.xml;
 
-
-
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -9,21 +7,21 @@ import java.util.Date;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 /**
- *
  * @author osa
  */
 public class XmlDateTimeAdapter extends XmlAdapter<String, Date> {
 
-    private DateFormat DF = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-    
+    private DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+
     @Override
     public Date unmarshal(String s) throws ParseException {
-        return DF.parse(s);
+        return df.parse(s);
     }
 
     @Override
     public String marshal(Date d) {
-        return DF.format(d);
+      System.err.println("... " + d);
+        return df.format(d);
     }
-    
+
 }
